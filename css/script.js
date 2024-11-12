1e84ce8dffecd12953f3d94cb92d453a8de360b5
otnayuno <nynniiqktmtn@gmail.com>
2024-11-12 19:29:48 +0900

    fv画像サンプル、テキストリンクスクロール調整

---
+++ css/script.js
@@ -0,0 +1,14 @@
+// ページスクロール調整
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - 95,
+      behavior: 'smooth',
+    });
+  });
+});
